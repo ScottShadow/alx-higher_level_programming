a7feabf9e5e3a5443d59d55963810b8acbbf5821
ScottShadow <rudaseswascottmc@gmail.com>
2023-11-28 20:37:23 +0000

    switched to .format

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,5 +6,5 @@ def uppercase(str):
     for char in str:
         if islower(char):
             char = chr(ord(char) - 32)
-        print(f"{char}", end='')
+        print("{}".format(char), end='')
     print("")
