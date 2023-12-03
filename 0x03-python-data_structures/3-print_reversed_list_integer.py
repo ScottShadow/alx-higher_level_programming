520eed3378c71204032aba7ea33196013601237c
ScottShadow <rudaseswascottmc@gmail.com>
2023-12-03 11:44:19 +0000

    added 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    my_list_rev = my_list.copy()
+    my_list_rev.reverse()
+    for element in my_list_rev:
+        print("{}".format(element))
