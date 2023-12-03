8409d97e8ff2a0757d3b9c0556bcdc47a0fce7bf
ScottShadow <rudaseswascottmc@gmail.com>
2023-12-03 12:11:41 +0000

    added 3

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if my_list is None:
+        return None
+    if 0 > idx >= len(my_list):
+        return my_list.copy()
+    else:
+        my_new_list = my_list.copy()
+        my_new_list[idx] = element
+        return my_new_list
