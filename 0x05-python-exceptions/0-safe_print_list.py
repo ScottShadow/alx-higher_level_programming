5a80b93ba3d383f0a7a467a4dfd67acb5a310755
ScottShadow <rudaseswascottmc@gmail.com>
2023-12-18 15:47:15 +0000

    fixed output

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     rez = 0
-    if x == 0:
-        print(None)
-        return 0
     try:
-        for i in range(x):
-            rez = rez*10 + my_list[i]
-        print(rez)
+        if x > 0:
+            for i in range(x):
+                rez = rez*10 + my_list[i]
+            print(rez)
         return (my_list[i])
     except IndexError:
         rez = 0
@@ -15,3 +13,6 @@ def safe_print_list(my_list=[], x=0):
             rez = rez*10 + i
         print(rez)
         return (i)
+    except UnboundLocalError:
+        print(None)
+        return 0
