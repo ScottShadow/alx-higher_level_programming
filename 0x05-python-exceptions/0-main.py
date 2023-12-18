78f49b61c42c84ca4bd9e6d7229c4b5aa640f404
ScottShadow <rudaseswascottmc@gmail.com>
2023-12-18 16:33:22 +0000

    tried exceptions with space

--- 0x05-python-exceptions/0-main.py
+++ 0x05-python-exceptions/0-main.py
@@ -3,5 +3,5 @@ safe_print_list = __import__('0-safe_print_list').safe_print_list
 
 my_list = [1, 2, 7, 4, 5, "haha", 7, "con"]
 
-nb_print = safe_print_list(my_list, "m")
+nb_print = safe_print_list(my_list, 0)
 print("nb_print: {:d}".format(nb_print))
--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,18 +2,13 @@
 def safe_print_list(my_list=[], x=0):
     i = 0
     size = 0
-    try:
-        if (x <= 0):
-            return 0
-        for i in range(x):
+    for i in range(x):
+        try:
             print(my_list[i], end="")
             size += 1
-        print()
-        return (size)
-    except IndexError:
-        for i in my_list:
-            pass
-        print()
-        return (size)
-    except Exception:
-        return 0
+        except IndexError:
+            break
+        except Exception:
+            break
+    print()
+    return (size)
