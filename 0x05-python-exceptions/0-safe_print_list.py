76854ade3ac554be5685600012ae2f2295718d66
ScottShadow <rudaseswascottmc@gmail.com>
2023-12-18 16:15:27 +0000

    hmmm

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -13,6 +13,5 @@ def safe_print_list(my_list=[], x=0):
             pass
         print()
         return i
-    except Exception as e:
-        print(f"Error: {e}")
+    except Exception:
         return 0
