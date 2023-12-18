bb4282ce51659ffdf75dd1e4c474fe4f3015a56d
ScottShadow <rudaseswascottmc@gmail.com>
2023-12-18 15:27:57 +0000

    initial commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    rez = 0
+    try:
+        for i in range(x):
+            rez = rez*10 + my_list[i]
+        print(rez)
+        return (rez)
+    except IndexError:
+        rez = 0
+        for i in my_list:
+            rez = rez*10 + i
+        print(rez)
+        return (rez)
