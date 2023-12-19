6cf41a3830b2c02005967a0f8b9cf2f2c1fe9cb2
ScottShadow <rudaseswascottmc@gmail.com>
2023-12-19 16:11:33 +0000

    initial commit

---
+++ 0x06-python-classes/5-main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+Square = __import__('5-square').Square
+
+my_square = Square(3)
+my_square.my_print()
+
+print("--")
+
+my_square.size = 5
+my_square.my_print()
+
+print("--")
+
+my_square.size = 8
+my_square.my_print()
+
+print("--")
