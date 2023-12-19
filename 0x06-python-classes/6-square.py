ef73b1da7df39aa805a1915678346a5289b7391b
ScottShadow <rudaseswascottmc@gmail.com>
2023-12-19 21:51:38 +0000

    added 101

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -65,8 +65,8 @@ class Square:
         if self.__size == 0:
             print()
             return
-        [print("") for i in range(0, self.__position[1])]
+        [print("_") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("=", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
             print("")
