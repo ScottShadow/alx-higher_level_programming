0765356d8f30fafd58695d602dc34d3a06b5f742
ScottShadow <rudaseswascottmc@gmail.com>
2023-12-19 20:57:35 +0000

    added documentation

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,7 +6,7 @@ class Square:
     def __init__(self, size=0):
         """Constructor Function
         Args:
-            size : lenght of side of square
+            size: lenght of side of square
         """
         if (type(size) is not int):
             raise TypeError("size must be an integer")
