760e0690aa4da5cb64092819b477bb923024cbff
ScottShadow <rudaseswascottmc@gmail.com>
2024-01-08 20:33:48 +0000

    codestyle 6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -2,11 +2,22 @@
 """
 Base Geometry Module
 
-Defines a basic geometric class, BaseGeometry.
+
+Class:
+    BaseGeometry - Base class for geometric objects.
+
+Methods:
+    area() - Raises an exception indicating it is not implemented.
+
+Usage:
+    Inherit from BaseGeometry to create specific geometric classes
+      and implement the area method.
 """
 
 
 class BaseGeometry:
+    """Defines a basic geometric class, BaseGeometry."""
+
     def area(self):
         """area class unimplemented"""
         raise Exception("area() is not implemented")
