30a714cc19ba6a49b2980d0f579db3e566b6d17c
ScottShadow <rudaseswascottmc@gmail.com>
2024-01-09 17:59:13 +0000

    added serializer2

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,6 +4,8 @@
 import json
 myclassmodule = __import__('8-my_class')
 MyClass = getattr(myclassmodule, 'MyClass')
+myclassmodule2 = __import__('8-my_class_2')
+MyClass2 = getattr(myclassmodule2, 'MyClass')
 
 
 def myclass_serializer(obj):
@@ -19,6 +21,20 @@ def myclass_serializer(obj):
     return {'name': obj.name, 'number': obj.number}
 
 
+def myclass_serializer2(obj):
+    """
+    Custom JSON serializer function for MyClass objects.
+
+    Args:
+        obj (MyClass): The object to be serialized.
+
+    Returns:
+        dict: A dictionary representing the serialized object.
+    """
+    return {'number': obj.number, '_MyClass__name': obj._MyClass__name,
+            'is_team_red': obj.is_team_red, 'score': obj.score}
+
+
 def class_to_json(obj):
     """
     Custom JSON serialization function for MyClass objects.
@@ -31,3 +47,5 @@ def class_to_json(obj):
     """
     if isinstance(obj, MyClass):
         return myclass_serializer(obj)
+    elif isinstance(obj, MyClass2):
+        return myclass_serializer2(obj)
