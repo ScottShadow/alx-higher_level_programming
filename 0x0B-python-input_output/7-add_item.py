f88f761b7f4897fe602a5b4689f5a6af248b59ac
ScottShadow <rudaseswascottmc@gmail.com>
2024-01-09 16:56:55 +0000

    added documentation

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,14 +11,11 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = 'add_item.json'
 
 try:
-    # Attempt to load existing data from the JSON file
     data = load_from_json_file(filename)
 except FileNotFoundError:
-    # If the file doesn't exist, initialize data as an empty list
     data = []
 
-# Extend the existing data with command-line arguments
+""" Extend the existing data with command-line arguments"""
 data.extend(sys.argv[1:])
 
-# Save the updated data back to the JSON file
 save_to_json_file(data, filename)
