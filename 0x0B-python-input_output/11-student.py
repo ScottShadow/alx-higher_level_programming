049ad16d49c1f5f04d8c14fcbae44b6e4f46e4fe
ScottShadow <rudaseswascottmc@gmail.com>
2024-01-09 20:44:56 +0000

    added 12

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,45 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+"""Student Class Module"""
+
+import json
+
+
+class Student:
+    """
+    Represents a student with first name, last name, and age attributes.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Constructor method for creating a Student object.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Converts the Student object to a dictionary.
+
+        Returns:
+            dict: A dictionary representing the attributes
+            of the Student object.
+        """
+        if attrs is None:
+            return vars(self)
+        new_dictionary = {}
+        for key in attrs:
+            if hasattr(self, key):
+                new_dictionary[key] = getattr(self, key)
+        return new_dictionary
+
+    def reload_from_json(self, json):
+        for key in json:
+            if hasattr(self, key):
+                setattr(self, key, json[key])
--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""Pascal's Triangle Generator Function Module"""
+
+
+def pascal_triangle(n):
+    """
+    Generates Pascal's Triangle up to the specified number of rows.
+
+    Args:
+        n (int): The number of rows to generate.
+
+    Returns:
+        list: A list of lists representing Pascal's Triangle.
+    """
+    n = n + 1
+    mylist = []
+    oldlist = []
+    for i in range(n):
+        newlist = []
+        for j in range(i):
+            if j == 0 or j == i - 1:
+                newlist.append(1)
+            else:
+                newlist.append(oldlist[j - 1] + oldlist[j])
+        oldlist = newlist
+        if i != 0:
+            mylist.append(oldlist)
+
+    return mylist
