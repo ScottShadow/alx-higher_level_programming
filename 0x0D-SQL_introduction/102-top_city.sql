d8f02ddd16e44a7f2bda0154607924a36490082c
ScottShadow <rudaseswascottmc@gmail.com>
2024-02-13 21:55:42 +0000

    added advanced

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- Displays 3 cities with the highest avg temp in July - August
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
+LIMIT 3;
