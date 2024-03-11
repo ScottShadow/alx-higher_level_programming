a989414814df35f3c8e178cdb29137c22a820be9
unknown <rudaseswascott@gmail.com>
2024-03-11 22:20:38 +0200

    added files

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+text = ["C is fun", "Python is cool", "JavaScript is amazing"];
+for (let i = 0; i < text.length; i++) {
+    const element = text[i];
+    console.log(element);
+}
---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const { argv } = require("node:process");
+if (argv.length < 3) {
+    if (typeof argv[2] != "number") console.log("Missing number of occurrences");
+} else {
+    for (let index = 0; index < argv[2]; index++) {
+        console.log("C is fun");
+    }
+}
