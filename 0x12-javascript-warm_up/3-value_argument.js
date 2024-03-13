7643b0008210ac4b9fa4209a6257016147057342
ScottShadow <rudaseswascottmc@gmail.com>
2024-03-13 21:36:05 +0000

    fixes

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-if (argv.length < 3) {
-  console.log('No argument');
+if (argv[2] == null) {
+    console.log('No argument');
 } else {
-  console.log(argv[2]);
+    console.log(argv[2]);
 }
