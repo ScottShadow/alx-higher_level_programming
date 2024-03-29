083fdceea80a1261e59fcb6a8cb992ded1ad35ee
unknown <rudaseswascott@gmail.com>
2024-03-29 20:20:27 +0200

    updated 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request to a URL, and displays the body of the response
-curl -s -L -X GET "${1}"
+curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1"
