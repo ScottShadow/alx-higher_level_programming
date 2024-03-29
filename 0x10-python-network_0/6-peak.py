b484f4661f9b094668b1d7a75cc8e6cc8c1f771b
unknown <rudaseswascott@gmail.com>
2024-03-29 19:52:10 +0200

    used peaked peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,30 +1,9 @@
-#!/usr/bin/python
-"""find_peak Module"""
+#!/usr/bin/python3
+"""Algorithm for list of integers."""
 
 
 def find_peak(list_of_integers):
-    """
-    Find the peak element in a list of integers using binary search.
-
-    Args:
-        list_of_integers (List[int]): A list of integers to search
-        for the peak.
-
-    Returns:
-        int: The peak element in the list.
-    """
-    if not list_of_integers:
-        return None
-
-    left = 0
-    right = len(list_of_integers) - 1
-
-    while left < right:
-        mid = (left + right) // 2
-
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            left = mid + 1
-        else:
-            right = mid
-
-    return list_of_integers[left]
+    """Find a peak in a list of unsorted integers."""
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
