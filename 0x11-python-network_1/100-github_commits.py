08e3fb811a01137632e1fd37e294ca0fdd0c6576
Scott Rudaseswa <rudaseswascottmc@gmail.com>
2024-04-01 20:20:18 +0200

    small fixes

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,19 +6,18 @@ Print all commits by: `<sha>: <author name>` (one by line)
 
 import sys
 import requests
+if __name__ == "__main__":
+    REPONAME = sys.argv[1]
+    AUTHOR = sys.argv[2]
+    URL = f"https://api.github.com/repos/{AUTHOR}/{REPONAME}/commits"
 
-REPONAME = sys.argv[1]
-AUTHOR = sys.argv[2]
-URL = f"https://api.github.com/repos/{AUTHOR}/{REPONAME}/commits"
+    RESULTS = requests.get(URL, timeout=5)
+    RESULTS_JSON = RESULTS.json()
+    size = len(RESULTS_JSON)
 
-RESULTS = requests.get(URL, timeout=5)
-RESULTS_JSON = RESULTS.json()
-size = len(RESULTS_JSON)
-
-
-for i in range(0, 10):
-    if i >= size:
-        break
-    sha = RESULTS_JSON[i].get('sha')
-    name = RESULTS_JSON[i].get('commit').get('author').get('name')
-    print(f'{sha}: {name}')
+    for i in range(0, 10):
+        if i >= size:
+            break
+        sha = RESULTS_JSON[i].get('sha')
+        name = RESULTS_JSON[i].get('commit').get('author').get('name')
+        print(f'{sha}: {name}')
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,10 +5,14 @@ POST request to the passed URL with the email as a parameter
 import sys
 import urllib.request
 
-URL = sys.argv[1]
-EMAIL = sys.argv[2]
-DATA = urllib.parse.urlencode({'email': EMAIL}).encode('ascii')
-REQ = urllib.request.Request(URL, DATA, method="POST")
+
 if __name__ == "__main__":
-    with urllib.request.urlopen(REQ) as response:
-        print(response.read().decode('utf-8'))
+    URL = sys.argv[1]
+    EMAIL = sys.argv[2]
+    try:
+        DATA = urllib.parse.urlencode({'email': EMAIL}).encode('ascii')
+        REQ = urllib.request.Request(URL, DATA, method="POST")
+        with urllib.request.urlopen(REQ) as response:
+            print(response.read().decode('utf-8'))
+    except Exception as e:
+        print("Error:", e)
