75f1485bbe6a2ede3320eae8ac7bafd39e4c463b
Scott Rudaseswa <rudaseswascottmc@gmail.com>
2024-04-01 20:52:26 +0200

    fixed exceptions

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,7 +6,6 @@ with the letter as a parameter.
 """
 
 import sys
-from json import JSONDecodeError
 import requests
 if __name__ == "__main__":
     q = "" if len(sys.argv) < 2 else sys.argv[1]
@@ -20,7 +19,5 @@ if __name__ == "__main__":
             print(f"[{RESULT_JSON.get('id')}] {RESULT_JSON.get('name')}")
         else:
             print("No result")
-    except JSONDecodeError:
+    except:
         print("Not a valid JSON")
-    except Exception as e:
-        print(f"Error code: {e}")
