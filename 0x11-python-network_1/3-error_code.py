e851f4d9606571274d1c8f189625b80744d3a211
Scott Rudaseswa <rudaseswascottmc@gmail.com>
2024-04-01 20:29:09 +0200

    reverted

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     try:
         with urllib.request.urlopen(URL) as response:
             print(response.read().decode('utf-8'))
-    except Exception as e:
-        print(f"Error code: {e}")
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
