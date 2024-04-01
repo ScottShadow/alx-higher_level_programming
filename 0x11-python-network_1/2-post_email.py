ace4803e969f8a8d6a05c77b053a0724155c6ea6
Scott Rudaseswa <rudaseswascottmc@gmail.com>
2024-04-01 19:47:39 +0200

    modded name == main

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,5 +9,6 @@ URL = sys.argv[1]
 EMAIL = sys.argv[2]
 DATA = urllib.parse.urlencode({'email': EMAIL}).encode('ascii')
 REQ = urllib.request.Request(URL, DATA, method="POST")
-with urllib.request.urlopen(REQ) as response:
-    print(response.read().decode('utf-8'))
+if __name__ == "__main__":
+    with urllib.request.urlopen(REQ) as response:
+        print(response.read().decode('utf-8'))
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,8 +4,9 @@ import sys
 import urllib.request
 
 URL = sys.argv[1]
-try:
-    with urllib.request.urlopen(URL) as response:
-        print(response.read().decode('utf-8'))
-except urllib.error.HTTPError as e:
-    print(f"Error code: {e.code}")
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(URL) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
